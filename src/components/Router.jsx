56725c2da5b7a5661f06b9914a02aa9d63583d8c
Kayyro <robin.estebe8@epitech.eu>
2026-05-19 17:14:31 +0200

    création page Error 404 avec lien retour accueil

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import Home from '../pages/Home';
+import Error from '../pages/Error';
 
 const Layout = () => {
   return (
@@ -19,7 +20,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    errorElement: <div>Page 404 (Erreur)</div>, // a remplacer par mon composent error
+    errorElement: <Error>Page 404 (Erreur)</Error>, // a remplacer par mon composent errorerrorElement: <Error />
     children: [
       {
         path: "/",
